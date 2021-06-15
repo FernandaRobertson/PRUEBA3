18554483dbb6768f7c7161527214d29e4e87c4f1
FernandaRobertson <80720044+FernandaRobertson@users.noreply.github.com>
2021-06-14 20:26:48 -0400

    todo

--- prueba3_1/core/models.py
+++ prueba3_1/core/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class Categoria(models.Model):
+    idCategoria =models.IntegerField(primary_key=True, verbose_name='Id de Categoria')
+    nombreCategoria = models.CharField(max_length=50, verbose_name='Nombre de la Categoria')
+
+    def __str__(self):
+        return self.nombreCategoria
+
+#modelo para libro
+
+class Libro(models.Model):
+    isbn= models.IntegerField( primary_key=True, verbose_name='ISBN')
+    nombre= models.CharField(max_length=30, verbose_name='Nombre Libro')
+    autor= models.CharField(max_length=40, verbose_name='Autor')
+    descripcion= models.CharField(max_length=350, verbose_name='Descripcion')
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+
+    def __int__(self):
+        return self.isbn
--- prueba3_1/core/views.py
+++ prueba3_1/core/views.py
@@ -1,5 +1,34 @@
 from django.shortcuts import render
+from .models import Libro
+from .form import LibroForm
 
 # Create your views here.
 def home(request):
+    
     return render(request, 'core/home.html')
+
+def libros(request):
+    libros = Libro.objects.all()
+    datos = {
+        'libros': libros
+    }
+    return render(request, 'core/libros.html', datos)
+
+def form_libro(request):
+    datos = {
+        'form': LibroForm()
+    }
+    if request.method == 'POST':
+        formulario = LibroForm(request.POST)
+        if formulario.is_valid:
+            formulario.save()
+            datos['mensaje'] = "Datos guardados correctamente"
+
+    return render(request, 'core/form_libros.html', datos)
+
+def mod_libros(request, id):
+    libros = Libro.objects.get(isbn=id)
+    datos = {
+        'mod': LibroForm(instance=libros)
+    }
+    return render(request, 'core/mod_libros.html', datos)
