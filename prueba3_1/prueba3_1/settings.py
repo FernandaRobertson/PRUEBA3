d91a0426a653abae81950dbef94a8d9a644f2c4e
FernandaRobertson <80720044+FernandaRobertson@users.noreply.github.com>
2021-06-30 22:28:07 -0400

    prueba4

--- prueba3_1/prueba3_1/settings.py
+++ prueba3_1/prueba3_1/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'core',
+    'rest_libro',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
@@ -70,6 +73,11 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'prueba3_1.wsgi.application'
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+}
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
